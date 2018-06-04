31146279179ca54c4487db7fc0aa342bab08ce46
Tony Sappe <22781949+tony-sappe@users.noreply.github.com>
2018-06-04 10:39:56 -0400

    Confirmed sorting works as expected for new functionality

--- django_mock_queries/query.py
+++ django_mock_queries/query.py
@@ -201,19 +201,18 @@ class MockSet(MagicMock):
             if type(field) == str:  # original, for simple orderby functions
                 is_reversed = field.startswith('-')
                 attr = field[1:] if is_reversed else field
-                results = sorted(results,
-                                 key=lambda r: get_attribute(r, attr),
-                                 reverse=is_reversed)
             else:
                 # for more complicated orderby functions which is type OrderBy()
                 try:
                     is_reversed = field.descending
                     attr = field.expression.name
-                    results = sorted(results,
-                             key=lambda r: get_attribute(r, attr),
-                             reverse=is_reversed)
                 except Exception as e:
                     raise Exception(e)
+
+        results = sorted(
+            results,
+            key=lambda r: get_attribute(r, attr),
+            reverse=is_reversed)
 
         return MockSet(*results, clone=self)
 
